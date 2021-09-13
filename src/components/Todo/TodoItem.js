8cfc1bcf3a454be24000ae877cef93354f9f81ed
Patrick Ojeh <patrickojeh@gmail.com>
2021-09-13 12:47:20 +0100

    fixed with toggling tasks. used State instead

--- src/components/Todo/TodoItem.js
+++ src/components/Todo/TodoItem.js
@@ -1,21 +1,28 @@
 import Styles from '../UI/Styles.module.css';
 import chevronIcon from '../../icon--chevron-down.svg';
+import {useState} from 'react';
 
 function TodoItem(props) {
+  let [isCollapsed, setIsCollapsed] = useState(false)
+
   function checkboxHandler(e) {
     let checkboxId = e.target.id;
     props.onCheckboxId(checkboxId);
   }
 
+  function toggleCollapseHandler(e) {
+    setIsCollapsed(!isCollapsed);
+  }
+
   return (
     <div>
-      <a href="#" className={Styles['todo-item']}>
+      <a href="#" className={`${Styles['todo-item']}`} onClick={toggleCollapseHandler}>
         <div>
           <input type="checkbox" id={props.id} onChange={checkboxHandler} checked={props.completed} />
         </div>
         <div>
           <p>{props.title}</p>
-          <div className={Styles.collapsed}>
+          <div className={`${Styles.collapsed} ${isCollapsed && Styles.expand}`}>
             <div>
               <span></span>
               <small>{props.description}</small>
