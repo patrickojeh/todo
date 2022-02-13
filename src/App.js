e5c592143f968735e7da3ad4ee3e4597ff648dc5
Patrick Ojeh <patrickojeh@gmail.com>
2022-02-13 18:58:01 +0100

    add Greenbii sdk

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import './App.css';
 import Styles from './components/UI/Styles.module.css';
 import Todo from './components/Todo/Todo';
 import Footer from './components/Todo/Footer';
+import { useEffect } from 'react';
+import {greenbiisdk} from 'greenbiisdk';
 
 function App() {
-  // let todosObj = [{
-  //   id: 1,
-  //   title: "",
-  //   description: "",
-  //   date: "",
-  //   completed: false
-  // }]
-
   const savedTodos = JSON.parse(localStorage.getItem('todos'));
   let todosObj = savedTodos ? savedTodos : [];
+  const API_KEY = 'dgm7dcn7fyflsgn7tlbh0ux7i0bvwkjfc7z3d6812thtk1e9t6fu2pqfj015pd2y';
+  useEffect(() => {
+    const greenbii = new greenbiisdk();
+
+    const param = new URLSearchParams(window.location.search);
+    greenbii.init(
+      {access_token: param.get("request_access_token", API_KEY)}
+    ).then(status=>{
+      if(status === true) {
+        console.log(greenbii.getCurrentUser());
+        // greenbii.getBusinessDetails()
+      }
+      else {
+        console.log(greenbii.getStatusMessage())
+      }
+    })
+  }, []);
 
   return (
     <div className={Styles.app}>
